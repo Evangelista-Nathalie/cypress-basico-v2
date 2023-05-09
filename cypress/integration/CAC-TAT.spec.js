cc94a900227068a727fbdca0db69e6510958fe82
Evangelista-Nathalie <nathalie.analista.qualidade@hotmail.com>
2023-05-09 19:15:39 -0300

    Finalizando curso

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -251,11 +251,28 @@ describe('Central de Atendimento ao Cliente TAT', function(){
         
     });
 
-    it.only('preenche a area de texto usando o comando invoke', () => {
+    it('preenche a area de texto usando o comando invoke', () => {
         const longText = Cypress._.repeat('teste ', '20')
 
         cy.get('#open-text-area')
         .invoke('val', longText)
         .should('have.value', longText)
+    });
+
+    it('faz uma requisição HTTP', () => {
+        cy.request('https://cac-tat.s3.eu-central-1.amazonaws.com/index.html')
+        .should((response) => {
+            const {status, statusText, body} = response
+            expect(status).to.equal(200);
+            expect(statusText).to.equal('OK')
+            expect(body).to.include('CAC TAT')
+        })
+    });
+
+    it('encontre o gato escondido', () => {
+        
+        cy.get('#cat')
+        .invoke('show')
+        .should('be.visible')
     });
 })
